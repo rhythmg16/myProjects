a21e6e8699d605e1612fbd3c95ae7b5421b6bb79
Rhythm Goyal <rhythmgoyal18@gmail.com>
2024-07-19 17:08:53 -0500

    Cleaning it up

--- quizApp/main.js
+++ quizApp/main.js
@@ -5,50 +5,130 @@ const questions = [
       {
         text: "Shark",
         correct: false,
-        msg: "WRONG, fun fact: They eat people in Florida",
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: They eat people in Florida",
       },
       {
         text: "Blue Whale",
         correct: true,
-        msg: "CORRECT, fun fact: They can reach speeds up to 30 mph in the water",
+        msg:
+          ' <i class="fa-solid fa-circle-check"></i>' +
+          "CORRECT, fun fact: They can reach speeds up to 30 mph in the water",
       },
       {
         text: "Elephant",
         correct: false,
-        msg: "WRONG, fun fact: They are very big animals",
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: They are very big animals",
       },
       {
         text: "Giraffe",
         correct: false,
-        msg: "WRONG, fun fact: They are very tall",
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: They are very tall",
       },
     ],
   },
   {
     question: "What is the smallest country in the world?",
     answers: [
-      { text: "Vatican City", correct: true },
-      { text: "Bhutan", correct: false },
-      { text: "Nepal", correct: false },
-      { text: "Shri Lanka", correct: false },
+      {
+        text: "Vatican City",
+        correct: true,
+        msg:
+          ' <i class="fa-solid fa-circle-check"></i>' +
+          "CORRECT, fun fact: the pope lives there",
+      },
+      {
+        text: "Bhutan",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: It is the worlds only carbon negative country",
+      },
+      {
+        text: "Nepal",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact:  It has the worlds highest mountains",
+      },
+      {
+        text: "Sri Lanka",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: It is one of the world's leading exporters in tea ",
+      },
     ],
   },
   {
     question: "What is the largest desert in the world?",
     answers: [
-      { text: "Kalahari", correct: false },
-      { text: "Gobi", correct: false },
-      { text: "Sahara", correct: false },
-      { text: "Antarctica", correct: true },
+      {
+        text: "Kalahari",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: It is the second largest desert in Africa",
+      },
+      {
+        text: "Gobi",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: It isn't sandy at all, it is mostly bare rock",
+      },
+      {
+        text: "Sahara",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: Many dinosaur fossils have been found in the Sahara desert",
+      },
+      {
+        text: "Antarctica",
+        correct: true,
+        msg:
+          ' <i class="fa-solid fa-circle-check"></i>' +
+          "CORRECT, fun fact: It is the largest ice-sheet in the world",
+      },
     ],
   },
   {
     question: "What is smallest continent in the world?",
     answers: [
-      { text: "Asia", correct: false },
-      { text: "Australia", correct: true },
-      { text: "Antarctica", correct: false },
-      { text: "Africa ", correct: false },
+      {
+        text: "Asia",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: It is home to over 60% of the population",
+      },
+      {
+        text: "Australia",
+        correct: true,
+        msg:
+          ' <i class="fa-solid fa-circle-check"></i>' +
+          "CORRECT, fun fact: It is home to the Great Barrier Reef",
+      },
+      {
+        text: "Antarctica",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          " WRONG, fun fact: There are about 5 million penguins in Antarctica",
+      },
+      {
+        text: "Africa ",
+        correct: false,
+        msg:
+          '<i class="fa-solid fa-circle-xmark"></i>' +
+          "WRONG, fun fact: Africa makes about 70% of the worlds cocoa beans",
+      },
     ],
   },
 ];
@@ -142,8 +222,6 @@ nextButton.addEventListener("click", () => {
 startQuiz();
 
 let toastBox = document.getElementById("toastBox");
-let successMsg = ' <i class="fa-solid fa-circle-check"></i>Good Job!';
-let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
 
 function showToast(msg, isCorrect) {
   console.log(msg);
@@ -152,7 +230,7 @@ function showToast(msg, isCorrect) {
   toast.innerHTML = msg;
   toastBox.appendChild(toast);
   if (isCorrect) {
-    toast.classList.add("goodJob");
+    toast.classList.add("correct");
     console.log("does it work");
   } else {
     toast.classList.add("wrong");
